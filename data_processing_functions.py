95aad9acbb13b48d8690b88f062754ef32c9c558
liyi201809 <liyi201809@gmail.com>
2019-05-06 23:32:53 +0200

    update the dpf_functions for SMOTE

--- data_processing_functions.py
+++ data_processing_functions.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+from operator import itemgetter
+from itertools import groupby
+import time
+
 def process_dict_reader(dictreader):
     dictlist = remove_refused(dictreader)
     dictlist = clean_formatting(dictlist)
@@ -7,14 +12,16 @@ def process_dict_reader(dictreader):
 
 def remove_refused(dictlist):
     result = []
+    count = 0
     for line in dictlist:
-        if line['simple_journal'] == 'refused':
+        if line['simple_journal'] == 'Refused':
+            count+=1
             continue
         if line['bin'] == 'na' or line['mail_id'] == 'na':
+            count+=1
             continue
-
         result.append(line)
-
+    print('deleted items:', count)
     return result
 
 
@@ -39,7 +46,6 @@ xdr_conversion_rates = {
     'GBP': 1.97804,
     'MXN': 26.4705,
     'AUD': 1.97804
-
 }
 
 def filter(dictlist, column, value):
@@ -83,3 +89,74 @@ def get_column(dictlist, column_name):
     for line in dictlist:
         result.append(line[column_name])
     return result
+
+def get_column_set(dictlist, column_name):
+    # returns a column as a list
+    result = []
+    for line in dictlist:
+        result.append([line[column_name]])
+    return result
+
+
+
+
+def get_cl_result(prediction, y_test):
+    TP, FP, FN, TN = 0, 0, 0, 0
+    for i in range(len(prediction)):
+        if y_test[i]==1 and prediction[i]==1:
+            TP += 1
+        if y_test[i]==0 and prediction[i]==1:
+            FP += 1
+        if y_test[i]==1 and prediction[i]==0:
+            FN += 1
+        if y_test[i]==0 and prediction[i]==0:
+            TN += 1
+    print ('TP: '+ str(TP))
+    print ('FP: '+ str(FP))
+    print ('FN: '+ str(FN))
+    print ('TN: '+ str(TN))
+    return TP,FP,FN, TN
+
+
+
+
+
+
+
+
+#%% for the example code in brightspace
+def aggregate(before_aggregate, aggregate_feature): #
+    if aggregate_feature == 'day':
+        after_aggregate = []
+        before_aggregate.sort(key = itemgetter(9))#sort by timestamp
+        temp = groupby(before_aggregate, itemgetter(9))
+        print(before_aggregate)
+        group_unit = []
+        mean = []
+        for i, item in temp:# i is group id
+            for jtem in item:# unit in each group
+                group_unit.append(jtem)
+            #for feature_i in xrange(6):
+            #    mean.append(zip(group_unit)[feature_i])
+            #after_aggregate.append(group_unit)
+            after_aggregate.append(mean)
+            group_unit = []
+        #print after_aggregate[0]
+        #print before_aggregate[0]
+    if aggregate_feature == 'client':
+        after_aggregate = []
+        pos_client = -3
+        before_aggregate.sort(key = itemgetter(pos_client))#sort with cardID firstly，if sort with 2 feature, itemgetter(num1,num2)
+        temp = groupby(before_aggregate, itemgetter(pos_client))#group
+        group_unit = []
+        for i, item in temp:# i is group id
+            for jtem in item:# unit in each group
+                group_unit.append(jtem)
+            after_aggregate.append(group_unit)
+            group_unit = []
+    return after_aggregate
+
+def string_to_timestamp(date_string):#convert time string to float value
+    time_stamp = time.strptime(date_string, '%m/%d/%Y %H:%M')
+    return time.mktime(time_stamp)
+
--- heatmap.py
+++ heatmap.py
@@ -1,69 +1,243 @@
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import math
-import data_processing_functions as dpf
-from data_processing_functions import process_dict_reader
+import data_processing_functions1 as dpf
+from data_processing_functions1 import process_dict_reader
+import datetime
+import random
 
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import OneHotEncoder
+
+from imblearn.over_sampling import RandomOverSampler
+###############################################################################
+#%% 1. Plot a histogram over the fraud and good transactions
 with open('data_for_student_case.csv') as file:
     reader = csv.DictReader(file)
-
     dictlist = process_dict_reader(reader)
-    # print(dpf.get_distinct_in_column(dictlist, 'currencycode'))
-    # for line in dictlist:
-    #     print(line)
 
-dictlist = dpf.filter(dictlist, 'currencycode', 'AUD')
-safe, fraud = dpf.split_label(dictlist)
-safe = dpf.get_column(safe, 'xdr_amount')
-fraud = dpf.get_column(fraud, 'xdr_amount')
+safe1, fraud1 = dpf.split_label(dictlist)
+safe = dpf.get_column(safe1, 'xdr_amount')
+fraud = dpf.get_column(fraud1, 'xdr_amount')
 safe.sort()
 fraud.sort()
+
 # relative size of fraud versus safe, assuming fraud is much smaller
 rel_size = len(safe)/len(fraud)
+
 # oversample fraud to create equal size
 original_fraud = fraud # for finding back original results
 oversampled_fraud = []
 for i in range(math.floor(rel_size)):
     oversampled_fraud.extend(fraud)
 
-
+# plot the histogrm of the oversampled fraud/non-fraud records
 bins = np.linspace(0, 60000, 100)
-
 plt.hist(safe, bins, alpha=0.5, color='green', label='safe')
 plt.hist(oversampled_fraud, bins, alpha=0.5, color='red', label='fraud')
+#plt.hist(original_fraud, bins, alpha=0.5, color='blue', label='fraud')
 plt.legend(loc='upper right')
 plt.xlabel('xdr amount')
 plt.ylabel('occurences')
 plt.show()
+#%% 2. Use the histogram to build a simple classifier
+fraud_count = 0   # predicted results
+safe_count = 0
 
-# fraud_count = 0
-# fraud_amount_caught = 0
-# fraud_amount = 0
-# safe_count = 0
-# for amount in safe:
-#     if amount>15000 and amount < 60000:
-#         safe_count += 1
-# for amount in fraud:
-#     if amount>15000 and amount < 60000:
-#         fraud_count += 1
+fraud_amount = 0  # real amount of frauded money 
+fraud_amount_caught = 0  # predicted amount of frauded money
+
+for amount in safe:
+     if amount>15000 and amount < 60000:
+         safe_count += 1
+for amount in fraud:
+     if amount>15000 and amount < 60000:
+         fraud_count += 1
+         fraud_amount_caught += amount
+     fraud_amount += amount
+
+ # safe_annoyed =100
+cost_to_annoy =100
+ # fraud_amount_caught = 10000
+ # total = fraud_amount_caught - safe_annoyed*cost_to_annoy
+
+print("at a cost of {}, we have a fraud detection accuracy of {} and {} of the total fraudulent cash flagged resulting in a performance of {}".format(
+     safe_count/len(safe)*100,
+     fraud_count/len(fraud)*100,
+       fraud_amount_caught/fraud_amount*100,
+     (fraud_amount_caught-safe_count*cost_to_annoy)/fraud_amount*100))
+
+#################################################################################
+#%% 3. Use sklearn kit to build classifiers on one feature only, xdr_amount
+safe_set = dpf.get_column_set(safe1, 'xdr_amount')
+fraud_set = dpf.get_column_set(fraud1, 'xdr_amount')
+
+# safe set with label
+safe_set = []
+for line in safe:
+    safe_set.append([int(line),0])
+# fraud set with label
+fraud_set = []
+for line in fraud:
+    fraud_set.append([int(line),1])
+# oversampled fraud set with label
+over_fraud_set = []
+for line in oversampled_fraud:
+    over_fraud_set.append([int(line),1])
+
+# combine and randomize the dataset for classifiers   
+wholeset = safe_set + fraud_set
+over_wholeset = safe_set + over_fraud_set
+random.shuffle(wholeset)
+random.shuffle(over_wholeset)
+
+# separate the label and feature sets
+dataset = [[item[0]] for item in wholeset]
+labelset = [item[1] for item in wholeset]
+over_dataset = [[item[0]] for item in over_wholeset]
+over_labelset = [item[1] for item in over_wholeset]
+
+#%% Use LR classifier to test data with/without oversampling
+# Test the oversampled dataset
+x_train, x_test, y_train, y_test = train_test_split(over_dataset, over_labelset, test_size = 0.2)
+l_clf = LogisticRegression()
+l_clf.fit(x_train,y_train)
+l_prediction = l_clf.predict(x_test)
+dpf.get_cl_result(l_prediction,y_test)
+
+# Test the original dataset
+x_train, x_test, y_train, y_test = train_test_split(dataset, labelset, test_size = 0.2)
+l_clf = LogisticRegression()
+l_clf.fit(x_train,y_train)
+l_prediction = l_clf.predict(x_test)
+dpf.get_cl_result(l_prediction,y_test)
+#%%
+from imblearn.over_sampling import SMOTE
+from sklearn.svm import LinearSVC
+import numpy as geek
+#%%
+X_resampled, y_resampled = SMOTE().fit_resample(x_train, y_train)
+#clf_smote = LinearSVC().fit(X_resampled, y_resampled)
+print(len(X_resampled))
+print(len(y_resampled))
+
+sm = SMOTE(random_state=42)
+X_res, y_res = sm.fit_resample(x_train, y_train)
+
+l_clf.fit(X_res,y_res)
+l_prediction = l_clf.predict(x_test)
+dpf.get_cl_result(l_prediction,y_test)
+
+##################################################################################
+#%% 4. Put all features/labels into a big datasets, basically pull from the example code in bright space
+(issuercountry_set, txvariantcode_set, currencycode_set, shoppercountry_set, interaction_set,
+verification_set, accountcode_set, mail_id_set, ip_id_set, card_id_set) = [set() for _ in range(10)]
+src = 'data_for_student_case.csv'
+ah = open(src, 'r')
+
+data=[]     #All categorical+numerical features.
+num_data=[] #only one numerical feature, amount.
+label=[]
+count_record = 0
+ah.readline()#skip first line
+for line_ah in ah:
+    if line_ah.strip().split(',')[9]=='Refused':# remove the row with 'refused' label, since it's uncertain about fraud
+        continue
+    if 'na' in str(line_ah.strip().split(',')[14]).lower() or 'na' in str(line_ah.strip().split(',')[4].lower()):
+        continue    
+    count_record += 1
+    # date reported flaud
+    bookingdate = dpf.string_to_timestamp(line_ah.strip().split(',')[1])
+    #country code
+    issuercountry = line_ah.strip().split(',')[2]
+    issuercountry_set.add(issuercountry)
+    #type of card: visa/master
+    txvariantcode = line_ah.strip().split(',')[3]
+    txvariantcode_set.add(txvariantcode)
+    #bin card issuer identifier
+    issuer_id = float(line_ah.strip().split(',')[4])
+    #transaction amount in minor units
+    amount = float(line_ah.strip().split(',')[5])
+    #the type of currentcy code
+    currencycode = line_ah.strip().split(',')[6]
+    currencycode_set.add(currencycode)
+    #country code
+    shoppercountry = line_ah.strip().split(',')[7]
+    shoppercountry_set.add(shoppercountry)
+    #online transaction or subscription
+    interaction = line_ah.strip().split(',')[8]
+    #define the fraud/legitible
+    interaction_set.add(interaction)
+    if line_ah.strip().split(',')[9] == 'Chargeback':
+        label_temp = 1#label fraud
+    else:
+        label_temp = 0#label save
+    #shopper provide CVC code or not    
+    verification = line_ah.strip().split(',')[10]
+    verification_set.add(verification)
+    #0 = Unknown, 1=Match, 2=No Match, 3-6=Not checked
+    cvcresponse = line_ah.strip().split(',')[11]
+    if int(cvcresponse,10) > 2:
+        cvcresponse = 3
+    #Date of transaction     
+    year_info = datetime.datetime.strptime(line_ah.strip().split(',')[12],'%m/%d/%Y %H:%M').year
+    month_info = datetime.datetime.strptime(line_ah.strip().split(',')[12],'%m/%d/%Y %H:%M').month
+    day_info = datetime.datetime.strptime(line_ah.strip().split(',')[12],'%m/%d/%Y %H:%M').day
+    creationdate = str(year_info)+'-'+str(month_info)+'-'+str(day_info)
+    #Date of transaction-time stamp
+    creationdate_stamp = dpf.string_to_timestamp(line_ah.strip().split(',')[12])
+    #merchant’s webshop
+    accountcode = line_ah.strip().split(',')[13]
+    accountcode_set.add(accountcode)
+    #mail
+    mail_id = int(float(line_ah.strip().split(',')[14].replace('email','')))
+    mail_id_set.add(mail_id)
+    #ip
+    ip_id = int(float(line_ah.strip().split(',')[15].replace('ip','')))
+    ip_id_set.add(ip_id)
+    #card
+    card_id = int(float(line_ah.strip().split(',')[16].replace('card','')))
+    card_id_set.add(card_id)
+    
+    data.append([issuercountry, txvariantcode, str(issuer_id), currencycode,
+                shoppercountry, interaction, verification, cvcresponse, str(creationdate_stamp),
+                 accountcode, str(mail_id), str(ip_id), str(card_id), creationdate, amount])# add the interested features here
+    num_data.append([amount])
+    label.append(label_temp)
+print('the final num of records:', count_record)
+
+
+#%% get a pure categorical feature dataset for classifier
+data_np = np.asarray(data) # remove the last column of num element
+cag_data = data_np[:,0:13]
+cag_data = np.array(cag_data).tolist()
+
+#%% encode the categorical features by onehorencoder
+X = cag_data[0:500] # due to memory restriction, only select part to implement
+enc = OneHotEncoder(handle_unknown='ignore')
+enc.fit(X)
+data444 = enc.transform(X).toarray()
+#%%
+
+
+
+
+
+
+
+
+
 #
-# for amount in fraud:
-#     if amount>15000 and amount < 60000:
-#         fraud_amount_caught += amount
 #
-#     fraud_amount += amount
 #
-# # safe_annoyed =100
-# cost_to_annoy =100
-# # fraud_amount_caught = 10000
-# # total = fraud_amount_caught - safe_annoyed*cost_to_annoy
-#
-# print("at a cost of {}, we have a fraud detection accuracy of {} and {} of the total fraudulent cash flagged resulting in a performance of {}".format(
-#     safe_count/len(safe)*100,
-#     fraud_count/len(fraud)*100,
-#       fraud_amount_caught/fraud_amount*100,
-#     (fraud_amount_caught-safe_count*cost_to_annoy)/fraud_amount*100))
-# # a = np.random.random((16, 16))
-# # plt.imshow(a, cmap='hot', interpolation='nearest')
-# #
+#x_train, x_test, y_train, y_test = train_test_split(data333, label, test_size = 0.2)
+#clf = SVC(kernel='linear')
+#clf.fit(x_train,y_train)
+#clf_predictions = clf.predict(x_test)
+#print("Accuracy: {}%".format(clf.score(x_test, y_test) * 100 ))
+
+
